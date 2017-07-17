45766a04e065adb7b1866782adc71596591a4854
Jay Price <jxprice@live.com>
2017-07-17 15:27:35 -0400

    First example. Inputted manually. Test concept.

--- js/maps.js
+++ js/maps.js
@@ -1,11 +1,30 @@
-function initMap() {
-	var uluru = {lat: 40.8068, lng: 131.044};
-	var map = new google.maps.Map(document.getElementById('map'), {
-		zoom: 7;
-		center: uluru;
-	});
-	var marker = new.googlemaps.Marker({
-		position: uluru,
-		map: map
-	});
-}
+      var map;
+			var videourl = "https://www.youtube.com/embed/HaoWlx4E-KI?autoplay=1";
+      function initMap() {
+				var myLatlng = {lat: 41.0688, lng: -73.8752};
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: myLatlng,
+          zoom: 8
+        });
+				
+				var marker = new google.maps.Marker({
+          position: myLatlng,
+          map: map,
+          title: 'Click to zoom'
+        });
+				
+				map.addListener('center_changed', function() {
+          // 3 seconds after the center of the map has changed, pan back to the
+          // marker.
+          window.setTimeout(function() {
+            map.panTo(marker.getPosition());
+          }, 3000);
+        });
+				
+				marker.addListener('click', function() {
+          map.setZoom(10);
+          map.setCenter(marker.getPosition());
+					video = document.getElementById("myvideo");
+					video.src = videourl;
+        });
+      }
